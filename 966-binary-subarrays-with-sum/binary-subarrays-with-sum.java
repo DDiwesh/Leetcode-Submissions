30ccc7c54dc8caf2008379837694fc975cdf0f74
DDiwesh <160175068+DDiwesh@users.noreply.github.com>
2024-06-23 23:26:31 +0530

    Time: 1 ms (100.00%) | Memory: 47.4 MB (68.27%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,16 +1,19 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n = nums.length, count = 0;
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum += nums[j];
-                if(sum==goal){
-                    count++;
-                }else if(sum>goal){
-                    break;
-                }
+       return (numArray(nums,goal)-numArray(nums,goal-1));
+    }
+    
+    public int numArray(int[] num, int goal){
+        if(goal<0) return 0;
+        int n = num.length, count = 0, l = 0, r=0,sum=0;
+        while(r<n){
+            sum += num[r];
+            while(sum > goal){
+                sum -= num[l];
+                l++;
             }
+            count += r-l+1;
+            r++;
         }
         return count;
     }
